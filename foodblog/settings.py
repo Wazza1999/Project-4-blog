a6947261b9bf6db701658989006d551544190e9a
Wazza1999 <warren.selby@hotmail.co.uk>
2025-03-26 11:41:32 +0000

    readies code for deploy to heroku

--- foodblog/settings.py
+++ foodblog/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-mfl!y3o8tdqo*-z)j6+9z!jljcuh0*b&a$msivemldea!kw#vb'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [".herokuapp.com", "127.0.0.1", "localhost"]
 
 
 # Application definition
