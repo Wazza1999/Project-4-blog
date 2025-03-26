1df63fd502f7002b22b6ff269e665d6bb69229bf
Wazza1999 <warren.selby@hotmail.co.uk>
2025-03-26 21:07:48 +0000

    started views file

--- foodblog/urls.py
+++ foodblog/urls.py
@@ -1,8 +1,7 @@
 from django.contrib import admin
-from django.urls import path
-from blog.views import my_blog
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('blog/', my_blog, name='blog')
+    path("", include("blog.urls"), name='blog-urls')
 ]
