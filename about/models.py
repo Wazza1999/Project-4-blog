c505a94df86069a518bc97b574f59f958f732d75
Wazza1999 <warren.selby@hotmail.co.uk>
2025-03-28 11:31:30 +0000

    created models for about app

--- about/models.py
+++ about/models.py
@@ -2,3 +2,14 @@ from django.db import models
 from django.contrib.auth.models import User
 
 # Create your models here.
+
+
+class About(models.Model):
+    title = models.Charfield(max_length=200, unique=True)
+    content = models.TextField(null=True)
+    created_on = models.DateTimeField(auto_now_add=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="about_posts")
+
+
+def __str__(self):
+    return f"{self.title}| written by {self.author}"
