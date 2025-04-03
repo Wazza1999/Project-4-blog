015793c80a71ddc23b16a37029cce872b9292e51
Wazza1999 <warren.selby@hotmail.co.uk>
2025-04-03 13:51:05 +0100

    added collaboration request functionality to the about page

--- about/views.py
+++ about/views.py
@@ -1,15 +1,21 @@
 from django.shortcuts import render
+from django.contrib import messages
 from .models import About
+from .forms import CollaborateForms
 
 
 def about_me(request):
-    """
-    Renders the About page
-    """
+
+    if request.method == "POST":
+        collaborate_form = CollaborateForms(data=request.POST)
+        if collaborate_form.is_valid():
+            collaborate_form.save()
+            messages.add_message(request, messages.SUCCESS, "Collaboration request recieved! I endeavour to respond within 2 working days")
     about = About.objects.all().order_by('-updated_on').first()
+    collaborate_form = CollaborateForms()
 
     return render(
         request,
         "about/about.html",
-        {"about": about},
+        {"about": about, "collaborate_form": collaborate_form},
     )
