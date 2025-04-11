f95cf19055a63063ad0570cf247b934efc9c1cfb
Wazza1999 <warren.selby@hotmail.co.uk>
2025-04-11 11:15:19 +0100

    fixed issue with database not saving more than one category

--- blog/models.py
+++ blog/models.py
@@ -14,7 +14,8 @@ class Category(models.Model):
 
     def save(self, *args, **kwargs):
         if not self.slug:
-            self.slug = self.name.lower().replace('', '-')
+            self.slug = self.name.lower().replace(' ', '-')
+        super().save(*args, **kwargs)
 
     def __str__(self):
         return self.name
